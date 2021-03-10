39016f7fbcdf28c393cb79929ce595c96401adf6
angelluis24 <70178924+angelluis24@users.noreply.github.com>
2021-03-10 18:23:16 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-public class hello_world{
+public class main{
     public static void main(String args[]){
       System.out.print("I like to eat bananas and play video games thanks for your time now you can go thanks a lot bye now if you allow me I'll stop writing") ;
     }
