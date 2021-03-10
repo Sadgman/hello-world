574ad90b596a4c28aec4c1189fe95899438fc6e8
angelluis24 <70178924+angelluis24@users.noreply.github.com>
2021-03-10 16:08:15 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from colorama import Fore, init
+
+print(Fore.RED + "\n I like to eat bananas and play video games thanks for your time now you can go thanks a lot bye now if you allow me I'll stop writing")
+
+init(autoreset=True)
