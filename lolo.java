9a3994adffd200c0a50b7b85ba6847804858ab88
angelluis24 <70178924+angelluis24@users.noreply.github.com>
2021-03-10 18:24:24 -0500

    Update and rename main.java to lolo.java

--- lolo.java
+++ lolo.java
@@ -1,4 +1,4 @@
-public class main{
+public class lolo{
     public static void main(String args[]){
       System.out.print("I like to eat bananas and play video games thanks for your time now you can go thanks a lot bye now if you allow me I'll stop writing") ;
     }
