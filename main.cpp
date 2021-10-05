ec01aceda92aab347dc69e12702747b6a74fed7e
angelluis24 <70178924+angelluis24@users.noreply.github.com>
2021-10-05 13:28:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
 
 int main(){
-    cout<<"Nothing\n";
-    system("PAUSE");
+    int num = 1;
+    while(num < 11){
+        cout<<num<<endl;
+        num += 1;
+        Sleep(1000);
+    }
+    cout<<"nothing";
 }
